b33ef79d0a3d54f588d38737c4df15298df5dada
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 02:34:18 +0000

    1.1.9

---
+++ tablesome/vendor/composer/installed.php
@@ -0,0 +1,57 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e428517d6dcc6f391ecb07658fa6f90f121a9b2f',
+    'name' => 'pauple/tablesome',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.12.0',
+      'version' => '1.12.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+    ),
+    'pauple/pluginator' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'b3a31b02c300130b3d6554b993e17dc9555a5404',
+    ),
+    'pauple/tablesome' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e428517d6dcc6f391ecb07658fa6f90f121a9b2f',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+  ),
+);
